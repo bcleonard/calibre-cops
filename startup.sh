65b0f6effdc12a9f37fb53a21fed879d4aab4a12
Bradley Leonard <bradley@stygianresearch.com>
2015-11-07 18:11:03 -0500

    updated php conf and startup files

---
+++ startup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#
+# add crontab for root
+#
+crontab /scripts/crontab
+
+#
+# start crond
+#
+/usr/sbin/crond
+
+#
+# start php-fpm
+/usr/sbin/php-fpm -D
+
+#
+# start nginx
+#
+/usr/sbin/nginx -g "daemon off;" -c /etc/nginx/nginx.conf
